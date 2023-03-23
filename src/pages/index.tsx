7148931e4883d53ef13bc6a7ec414dbfcf22e896
NewmarLucas <newmarlucas@gmail.com>
2023-03-22 20:44:20 -0400

    instalando material ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
 import styles from '@/styles/Home.module.css';
 import HeadComponent from '@/components/Head';
+import { Button, Typography, Box } from '@mui/material';
 
 export default function Home() {
   return (
     <>
       <HeadComponent title='Home' />
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <Typography>Quer ir no meu casamento?</Typography>
+        <Box display={'flex'} flexDirection={'row'} gap={4} marginTop={2}>
+          <Button variant='contained'>negar</Button>
+          <Button variant='contained'>Aceitar</Button>
+        </Box>
+      </main>
     </>
   );
 }
