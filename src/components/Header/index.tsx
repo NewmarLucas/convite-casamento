cdf843911d8c1c0284243a65304192b7f27cf7fe
NewmarLucas <newmarlucas@gmail.com>
2023-03-23 22:06:41 -0400

    removendo bibliotecas de componentes e criando componente de header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+export function Header() {
+  return <header className={styles.header}>oi</header>;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,18 @@
 import styles from '@/styles/Home.module.css';
 import HeadComponent from '@/components/Head';
-import { Button, Typography, Box } from '@mui/material';
+import circle from '@/assets/circle.svg';
+import Image from 'next/image';
+import { Header } from '@/components/Header';
 
 export default function Home() {
   return (
     <>
       <HeadComponent title='Home' />
+      <Header />
       <main className={styles.main}>
-        <Typography>Quer ir no meu casamento?</Typography>
-        <Box display={'flex'} flexDirection={'row'} gap={4} marginTop={2}>
-          <Button variant='contained'>negar</Button>
-          <Button variant='contained'>Aceitar</Button>
-        </Box>
+        <div className={styles.rectangle} />
+        <Image src={circle} alt='circulo' className={styles.circleImg} />
+        <p>oi</p>
       </main>
     </>
   );
