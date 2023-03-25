5b5632abeb52312a7991fada7041d96565d960cc
NewmarLucas <newmarlucas@gmail.com>
2023-03-24 21:52:24 -0400

    adicionando botões e imagem no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import Image from 'next/image';
+
+import logo from '@/assets/logo.svg';
 import styles from './styles.module.css';
 
 export function Header() {
-  return <nav className={styles.header}>oi</nav>;
+  return (
+    <nav className={styles.header}>
+      <div className={styles.content}>
+        <Image src={logo} alt='logo' />
+        <div className={styles.buttonsContainer}>
+          <button className='ghost-button'>Presentes</button>
+          <button className='ghost-button'>Vestimenta</button>
+          <button className='ghost-button'>Contato</button>
+          <button className='filled-button'>CONFIRMAR PRESENÇA</button>
+        </div>
+      </div>
+    </nav>
+  );
 }
